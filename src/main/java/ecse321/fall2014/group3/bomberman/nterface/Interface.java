6b7b8fbb05ac8031507dc64b2dc314c2fff1071f
Aleksi Sapon <alek42008@yahoo.com>
2014-11-02 15:43:27 -0500

    Render entities

--- src/main/java/ecse321/fall2014/group3/bomberman/nterface/Interface.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/nterface/Interface.java
@@ -1,13 +1,16 @@
 package ecse321.fall2014.group3.bomberman.nterface;
 
 import java.nio.ByteBuffer;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import com.flowpowered.commons.ticking.TickingElement;
 import com.flowpowered.math.vector.Vector2f;
 
 import ecse321.fall2014.group3.bomberman.Game;
+import ecse321.fall2014.group3.bomberman.physics.entity.Entity;
 import ecse321.fall2014.group3.bomberman.world.Map;
 import ecse321.fall2014.group3.bomberman.world.tile.Tile;
 import org.lwjgl.LWJGLException;
@@ -45,6 +48,7 @@ import org.spout.renderer.lwjgl.LWJGLUtil;
 public class Interface extends TickingElement {
     private static final int WIDTH = 640, HEIGHT = 480;
     private static final float ASPECT_RATIO = WIDTH / (float) HEIGHT;
+    private static final float VIEW_WIDTH = 10 * ASPECT_RATIO, VIEW_HEIGHT = 10;
     private static final int SPRITE_SIZE = 64;
     private final Game game;
     private final Camera orthoCamera;
@@ -53,12 +57,14 @@ public class Interface extends TickingElement {
     private Pipeline pipeline;
     private long mapVersion = 0;
     private final Set<Model> tileModels = new HashSet<>();
+    private final java.util.Map<Entity, Model> entityModels = new HashMap<>();
 
     public Interface(Game game) {
         super("Interface", 60);
         this.game = game;
 
-        orthoCamera = Camera.createOrthographic(ASPECT_RATIO * 10, 0, 10, 0, 0, 10);
+        orthoCamera = Camera.createOrthographic(VIEW_WIDTH, 0, VIEW_HEIGHT, 0, 0, 10);
+        pipeline = new PipelineBuilder().clearBuffer().useCamera(orthoCamera).renderModels(tileModels).renderModels(entityModels.values()).updateDisplay().build();
     }
 
     @Override
@@ -105,8 +111,6 @@ public class Interface extends TickingElement {
         spriteVertexArray.setData(MeshGenerator.generatePlane(Vector2f.ONE));
 
         spriteModel = new Model(spriteVertexArray, spriteMaterial);
-
-        pipeline = new PipelineBuilder().clearBuffer().useCamera(orthoCamera).renderModels(tileModels).updateDisplay().build();
     }
 
     @Override
@@ -116,7 +120,7 @@ public class Interface extends TickingElement {
         }
 
         final Map map = game.getWorld().getMap();
-        long newVersion = map.getVersion();
+        final long newVersion = map.getVersion();
 
         if (mapVersion < newVersion) {
             tileModels.clear();
@@ -132,12 +136,33 @@ public class Interface extends TickingElement {
             mapVersion = newVersion;
         }
 
+        final Set<Entity> entities = game.getPhysics().getEntities();
+        for (Iterator<Entity> iterator = entityModels.keySet().iterator(); iterator.hasNext(); ) {
+            if (!entities.contains(iterator.next())) {
+                iterator.remove();
+            }
+        }
+        for (Entity entity : entities) {
+            Model model = entityModels.get(entity);
+            if (model == null) {
+                model = spriteModel.getInstance();
+                model.getUniforms().add(new IntUniform("spriteNumber", 0));
+                entityModels.put(entity, model);
+            }
+            model.setPosition(entity.getPosition().toVector3(0));
+            model.getUniforms().<IntUniform>get("spriteNumber").set(entity.getSpriteInfo().getSpriteNumber());
+        }
+
+        orthoCamera.setPosition(game.getPhysics().getPlayer().getPosition().toVector3().sub(VIEW_WIDTH / 2, VIEW_HEIGHT / 2, 0));
+
         pipeline.run(context);
     }
 
     @Override
     public void onStop() {
         context.destroy();
+        mapVersion = 0;
+        tileModels.clear();
     }
 
     private static GLImplementation getBestImpl() {
@@ -158,7 +183,8 @@ public class Interface extends TickingElement {
                 // maybe use caustic-software in this case
             }
         } catch (LWJGLException ex) {
-            throw new IllegalStateException("Error when initializing temporary OpenGL context", ex);
+            // try GL2.0 if we can't verify the supported version
+            return LWJGLUtil.GL20_IMPL;
             // maybe use caustic-software in this case
         }
     }
