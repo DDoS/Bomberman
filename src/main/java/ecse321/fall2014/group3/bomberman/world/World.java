94a2ee2ebae447df58326b16ffd91e914aa14ecc
phil douyon <phild.email@gmail.com>
2014-11-01 20:09:20 -0400

    Revert "fixed formatting?"
    This reverts commit 7751ac2b6c009bfeffa87ae368397214a6c316ee.

--- src/main/java/ecse321/fall2014/group3/bomberman/world/World.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/World.java
@@ -8,30 +8,30 @@ import ecse321.fall2014.group3.bomberman.Game;
  *
  */
 public class World extends TickingElement {
-	private final Game game;
-	private Map map;
+    private final Game game;
+    private Map map;
 
-	public World(Game game) {
-		super("World", 20);
-		this.game = game;
-	}
+    public World(Game game) {
+        super("World", 20);
+        this.game = game;
+    }
 
-	@Override
-	public void onStart() {
-		map = new Map();
-	}
+    @Override
+    public void onStart() {
+        map = new Map();
+    }
 
-	@Override
-	public void onTick(long l) {
+    @Override
+    public void onTick(long l) {
 
-	}
+    }
 
-	@Override
-	public void onStop() {
+    @Override
+    public void onStop() {
 
-	}
+    }
 
-	public Map getMap() {
-		return map;
-	}
+    public Map getMap() {
+        return map;
+    }
 }
--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Air.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Air.java
@@ -1,7 +1,7 @@
 package ecse321.fall2014.group3.bomberman.world.tile;
 
 public class Air extends Tile {
-	public Air() {
+    public Air() {
 
-	}
+    }
 }
