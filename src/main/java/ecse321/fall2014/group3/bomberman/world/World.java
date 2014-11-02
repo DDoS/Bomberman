f24e10a6d5eab088c4b479af36ebbeb93b2fa338
Aleksi Sapon <alek42008@yahoo.com>
2014-11-02 12:19:52 -0500

    Init map in World onStart

--- src/main/java/ecse321/fall2014/group3/bomberman/world/World.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/World.java
@@ -3,13 +3,15 @@ package ecse321.fall2014.group3.bomberman.world;
 import com.flowpowered.commons.ticking.TickingElement;
 
 import ecse321.fall2014.group3.bomberman.Game;
+import ecse321.fall2014.group3.bomberman.world.tile.Air;
+import ecse321.fall2014.group3.bomberman.world.tile.Wall;
 
 /**
  *
  */
 public class World extends TickingElement {
     private final Game game;
-    private Map map;
+    private final Map map = new Map();
 
     public World(Game game) {
         super("World", 20);
@@ -18,11 +20,20 @@ public class World extends TickingElement {
 
     @Override
     public void onStart() {
-        map = new Map();
+        for (int y = 0; y < Map.HEIGHT; y++) {
+            for (int x = 0; x < Map.WIDTH; x++) {
+                if (y == 0 || y == Map.HEIGHT - 1 || x == 0 || x == Map.WIDTH - 1
+                        || y % 2 == 0 || x % 2 == 0) {
+                    map.setTile(x, y, Wall.class);
+                } else {
+                    map.setTile(x, y, Air.class);
+                }
+            }
+        }
     }
 
     @Override
-    public void onTick(long l) {
+    public void onTick(long dt) {
 
     }
 
