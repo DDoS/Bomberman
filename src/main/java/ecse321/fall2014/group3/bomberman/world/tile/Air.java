3c5b4d81b8c0abd680a191db4a34aef182a57673
Aleksi Sapon <alek42008@yahoo.com>
2014-11-02 13:04:25 -0500

    Use temp sprites that match the wall and grass colors

--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Air.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Air.java
@@ -6,7 +6,7 @@ import com.flowpowered.math.vector.Vector2i;
 import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
 
 public class Air extends Tile {
-    private static final SpriteInfo AIR_SPRITE = new SpriteInfo("main", 0, Vector2i.ONE);
+    private static final SpriteInfo AIR_SPRITE = new SpriteInfo("main", 64, Vector2i.ONE);
 
     Air(Vector2f position) {
         super(position, null);
--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Wall.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Wall.java
@@ -6,7 +6,7 @@ import com.flowpowered.math.vector.Vector2i;
 import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
 
 public class Wall extends Tile {
-    private static final SpriteInfo WALL_SPRITE = new SpriteInfo("main", 1, Vector2i.ONE);
+    private static final SpriteInfo WALL_SPRITE = new SpriteInfo("main", 192, Vector2i.ONE);
 
     Wall(Vector2f position) {
         super(position, Tile.COLLISION_BOX);
