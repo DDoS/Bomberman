7fa1720a87cb4497ae30d09adbff11ad19cefd3f
marcomang <marco.manglaviti@gmail.com>
2014-11-09 14:18:07 -0500

    Change Login for Menu Usage

--- src/main/java/ecse321/fall2014/group3/bomberman/database/Login.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/database/Login.java
@@ -50,11 +50,11 @@ public class Login {
             switch (choice) {
 
                 case 1:
-                    createAccount();
+                    createAccount("test", "test");
                     break;
 
                 case 2:
-                    login();
+                    login("abc", "abc");
                     break;
 
                 default:
@@ -70,11 +70,15 @@ public class Login {
         System.out.println("Process Complete");
     }
 
-    public static void createAccount() {
+
+    public static boolean createAccount(String user, String password) {
+
 
         Connection c = null;
         Statement stmt = null;
         Scanner scan = new Scanner(System.in);
+        boolean success = false;
+
 
         try {
 
@@ -86,25 +90,26 @@ public class Login {
             boolean goodUser = false;
             String usr = null;
 
-            while (true) {
-                System.out.println("Enter a Username");
-                usr = scan.nextLine();
 
-                //Check if username already exists
 
-                ResultSet rs = stmt.executeQuery(" SELECT username FROM USERS WHERE username='" + usr + "';");
+            // System.out.println("Enter a Username");
+            //  usr = scan.nextLine();
+            usr = user;
+            //Check if username already exists
 
-                if (rs.next()) {
+            ResultSet rs = stmt.executeQuery(" SELECT username FROM USERS WHERE username='" + usr + "';");
 
-                    System.out.println("Username already exists :(");
-                } else {
+            if (rs.next()) {
 
-                    break;
-                }
+                System.out.println("Username already exists :(");
+                success = false;
+                return success;
+
             }
 
-            System.out.println("Enter a Password");
-            String pass = scan.nextLine();
+            //System.out.println("Enter a Password");
+            // String pass = scan.nextLine();
+            String pass = password;
 
             String sql = "INSERT INTO USERS (USERNAME,PASSWORD) " +
                     " VALUES ('" + usr + "', '" + pass + "' );";
@@ -114,38 +119,47 @@ public class Login {
             stmt.close();
             c.setAutoCommit(true);
             c.close();
+            success = true;
+
+
         } catch (Exception e) {
 
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
+
         }
 
         System.out.println("Create Account Success");
+        return success;
+
     }
 
-    public static void login() {
+    public static boolean login(String user, String password) {
 
         Connection c = null;
         Statement stmt = null;
         Scanner scan = new Scanner(System.in);
+        boolean good = false;
 
         try {
+
 
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:test.db");
             c.setAutoCommit(false);
             System.out.println("Opened DataBase Successfully");
 
-            //user attempts to login
-            System.out.println("Enter Username: ");
-            String userName = scan.nextLine();
 
-            System.out.println("Enter Password: ");
-            String userPass = scan.nextLine();
+            //user attempts to login
+            // System.out.println("Enter Username: ");
+            // String userName = scan.nextLine();
+
+            //  System.out.println("Enter Password: ");
+            // String userPass = scan.nextLine();
 
             stmt = c.createStatement();
 
-            String que = "SELECT * FROM USERS WHERE EXISTS (SELECT * FROM USERS WHERE username='" + userName + "');";
+            String que = "SELECT * FROM USERS WHERE EXISTS (SELECT * FROM USERS WHERE username='" + user + "');";
             String testPass = null;
 
             //GOOD QUERY
@@ -155,36 +169,42 @@ public class Login {
             if (!rs.next()) {
 
                 System.out.println("Username does not exist");
-                System.exit(0);
+                // System.exit(0);
+                good = false;
             }
 
-            boolean good = false;
+
 
             while (rs.next()) {
 
+
                 testPass = rs.getString("password");
 
-                if (testPass.equals(userPass)) {
+                if (testPass.equals(password)) {
 
                     System.out.println("Login Successful");
                     good = true;
 
                     break;
+
                 }
+
             }
 
-            if (!good) {
-                System.out.println("Login Unsuccessful");
-            }
             rs.close();
             stmt.close();
             c.close();
 
-            System.exit(0);
+
+
+
         } catch (Exception e) {
 
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
+
         }
+
+        return good;
     }
 }
