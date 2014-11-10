2542ffa16f3a1a51e324b52cfa2db0576c2baaa1
Aleksi Sapon <QcTechs@gmail.com>
2014-11-10 14:59:27 -0500

    Fix the Tiles and Entities not following the UML plan
    @philippe-d Read the f*cking UML diagram
    
    https://docs.google.com/document/d/1DuSoo7IM4v0ZIIGvr20OkgEPThDTwc-EGy4z
    wwoGoQ8/edit

--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Fire.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/Fire.java
@@ -1,31 +1,23 @@
 package ecse321.fall2014.group3.bomberman.world.tile;
 
-import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
 import com.flowpowered.math.vector.Vector2f;
 
+import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
+
 public class Fire extends LimitedLifetimeTile {
+    private static final SpriteInfo FIRE_SPRITE = new SpriteInfo("terrain", 1, Vector2f.ONE);
 
-    private static final SpriteInfo fire_SPRITE = new SpriteInfo("main", 1, Vector2f.ONE);
-    private int time = 0;
-    private int size = 0;
-
-    public Fire(Vector2f position, int t, int s) {
+    public Fire(Vector2f position) {
         super(position);
-        this.time = t;
-        this.size = s;
     }
 
     @Override
     public SpriteInfo getSpriteInfo() {
-        return fire_SPRITE;
+        return FIRE_SPRITE;
     }
 
-    public int getTime() {
-        return time;
+    @Override
+    public int getRemainingTime() {
+        return 0;
     }
-
-    public int getSize() {
-        return size;
-    }
-
 }
