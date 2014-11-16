f20d7c4b735abe93828f87866712e514c03c8f6f
Aleksi Sapon <alek42008@yahoo.com>
2014-11-16 14:55:32 -0500

    Add better map generation
    Has variable density and uses gradient coherent noise to provide more
    interesting features than pure random.

--- src/main/java/ecse321/fall2014/group3/bomberman/world/World.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/World.java
@@ -5,12 +5,13 @@ import ecse321.fall2014.group3.bomberman.ticking.TickingElement;
 import ecse321.fall2014.group3.bomberman.world.tile.Air;
 import ecse321.fall2014.group3.bomberman.world.tile.Breakable;
 import ecse321.fall2014.group3.bomberman.world.tile.Wall;
+import net.royawesome.jlibnoise.NoiseQuality;
+import net.royawesome.jlibnoise.module.source.Perlin;
 
 /**
  *
  */
 public class World extends TickingElement {
-    private static final float BREAKABLE_WALL_THRESHOLD = 0.3f;
     private final Game game;
     private final Map map = new Map();
 
@@ -21,27 +22,11 @@ public class World extends TickingElement {
 
     @Override
     public void onStart() {
-        for (int y = 0; y < Map.HEIGHT; y++) {
-            for (int x = 0; x < Map.WIDTH; x++) {
-                if (y == 0 || y == Map.HEIGHT - 1 || x == 0 || x == Map.WIDTH - 1
-                        || y % 2 == 0 && x % 2 == 0) {
-                    map.setTile(x, y, Wall.class);
-                } else {
-                    // TODO: generate this better
-                    if (y < 2 || x < 2 || Math.random() > BREAKABLE_WALL_THRESHOLD) {
-                        map.setTile(x, y, Air.class);
-                    } else {
-                        map.setTile(x, y, Breakable.class);
-                    }
-                }
-            }
-        }
-        map.incrementVersion();
+        generateMap(0.5);
     }
 
     @Override
     public void onTick(long dt) {
-
     }
 
     @Override
@@ -51,5 +36,40 @@ public class World extends TickingElement {
 
     public Map getMap() {
         return map;
+    }
+
+    private void generateMap(double density) {
+        // Invert the block density to get the air density
+        density = 1 - density;
+        // Seed a perlin noise generator to the current time
+        final Perlin perlin = new Perlin();
+        perlin.setSeed((int) System.currentTimeMillis());
+        perlin.setNoiseQuality(NoiseQuality.BEST);
+        // 0.3 provides features about 3 tiles in size
+        perlin.setFrequency(0.3);
+        // After 3 octaves, the frequency is 1.2, which is smaller than a tile
+        perlin.setOctaveCount(3);
+        // Generate the breakable and unbreakable walls
+        for (int y = 0; y < Map.HEIGHT; y++) {
+            for (int x = 0; x < Map.WIDTH; x++) {
+                if (y == 0 || y == Map.HEIGHT - 1 || x == 0 || x == Map.WIDTH - 1
+                        || y % 2 == 0 && x % 2 == 0) {
+                    map.setTile(x, y, Wall.class);
+                } else {
+                    // Normalize the value from (-1, 1) to (0, 1)
+                    if ((perlin.getValue(x, y, 0) + 1) / 2 >= density) {
+                        map.setTile(x, y, Breakable.class);
+                    } else {
+                        map.setTile(x, y, Air.class);
+                    }
+                }
+            }
+        }
+        // Make the starting position air
+        map.setTile(1, 1, Air.class);
+        map.setTile(2, 1, Air.class);
+        map.setTile(1, 2, Air.class);
+        // Signal a new map version to the physics and rendering
+        map.incrementVersion();
     }
 }
