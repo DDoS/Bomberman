12a7c70d27cd590d7751adeb4f39231c9bf44935
phil douyon <phild.email@gmail.com>
2014-11-18 20:05:32 -0500

    seperated new and returning users on login screen.

--- src/main/java/ecse321/fall2014/group3/bomberman/App.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/App.java
@@ -46,26 +46,53 @@ public class App {
         final JFrame frame = new JFrame("Login");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel(new GridLayout(6, 2));
+        JPanel panel = new JPanel(new GridLayout(20, 4));
         frame.add(panel, BorderLayout.CENTER);
+        
+        
+        
+        JLabel userLabel = new JLabel("User");
+        panel.add(userLabel);
+        
+        final JTextField userText = new JTextField(10);
+        panel.add(userText);
+        
+        JLabel passwordLabel = new JLabel("Password");
+        panel.add(passwordLabel);
+
+        final JPasswordField passwordText = new JPasswordField(10);
+        panel.add(passwordText);
+        
+        JButton loginButton = new JButton("login");
+        panel.add(loginButton);
+        panel.getRootPane().setDefaultButton(loginButton);
+        
+        panel.add(new JLabel());
+        
+        JLabel newLabel = new JLabel("NEW USER");
+        panel.add(newLabel);
+        
+        panel.add(new JLabel());
 
         JLabel realNameLabel = new JLabel("Real Name");
         panel.add(realNameLabel);
 
         final JTextField realNameText = new JTextField(10);
         panel.add(realNameText);
+        
+        JLabel newUserLabel = new JLabel("User");
+        panel.add(newUserLabel);
+        
+        final JTextField newUserText = new JTextField(10);
+        panel.add(newUserText);
+        
+        JLabel newPasswordLabel = new JLabel("Password");
+        panel.add(newPasswordLabel);
 
-        JLabel userLabel = new JLabel("User");
-        panel.add(userLabel);
-
-        final JTextField userText = new JTextField(10);
-        panel.add(userText);
-
-        JLabel passwordLabel = new JLabel("Password");
-        panel.add(passwordLabel);
-
-        final JPasswordField passwordText = new JPasswordField(10);
-        panel.add(passwordText);
+        final JPasswordField newPasswordText = new JPasswordField(10);
+        panel.add(newPasswordText);
+        
+        
 
         JLabel verifyLabel = new JLabel("Verify Password");
         panel.add(verifyLabel);
@@ -73,9 +100,7 @@ public class App {
         final JPasswordField verifyText = new JPasswordField(10);
         panel.add(verifyText);
 
-        JButton loginButton = new JButton("login");
-        panel.add(loginButton);
-        panel.getRootPane().setDefaultButton(loginButton);
+        
 
         JButton createButton = new JButton("New");
         panel.add(createButton);
@@ -88,15 +113,6 @@ public class App {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                if (!Arrays.equals(passwordText.getPassword(), verifyText.getPassword())) {
-                    passwordText.setText("");
-                    verifyText.setText("");
-                    error.setText("passwords dont match");
-                    error.setVisible(true);
-                } else {
-                    //TODO: add real name to database
-                    //if(Login.login(realNameText.getText(),userText.getText(), String.valueOf(passwordText.getPassword()), connection))
                     if (Login.login(userText.getText(), String.valueOf(passwordText.getPassword()), session)) {
                         loginWait.release();
                         session.create(userText.getText());
@@ -107,7 +123,6 @@ public class App {
                         error.setText("wrong username or password");
                         error.setVisible(true);
                     }
-                }
             }
         });
 
@@ -121,7 +136,7 @@ public class App {
                     error.setVisible(true);
                 } else {
                     //TODO: add real name to database
-                    //if(Login.login(realNameText.getText(),userText.getText(), String.valueOf(passwordText.getPassword()), connection))
+                    //if(Login.login(realNameText.getText(),newUserText.getText(), String.valueOf(newPasswordText.getPassword()), connection))
                     if (Login.createAccount(userText.getText(), String.valueOf(passwordText.getPassword()), session)) {
                         session.create(userText.getText());
                         loginWait.release();
