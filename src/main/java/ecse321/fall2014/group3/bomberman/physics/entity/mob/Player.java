34b1d8f008db55a32879e4d055eedbe65fb3fe51
Aleksi Sapon <QcTechs@gmail.com>
2014-11-20 17:13:41 -0500

    Finish player power management up implementation

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/mob/Player.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/mob/Player.java
@@ -9,7 +9,10 @@ import com.flowpowered.math.vector.Vector2f;
 import ecse321.fall2014.group3.bomberman.Direction;
 import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
 import ecse321.fall2014.group3.bomberman.physics.CollisionBox;
+import ecse321.fall2014.group3.bomberman.world.tile.powerup.BombUpgrade;
+import ecse321.fall2014.group3.bomberman.world.tile.powerup.FlameUpgrade;
 import ecse321.fall2014.group3.bomberman.world.tile.powerup.PowerUP;
+import ecse321.fall2014.group3.bomberman.world.tile.powerup.SpeedUpgrade;
 
 public class Player extends Mob {
     private static final Vector2f SIZE = Vector2f.ONE.mul(0.75f);
@@ -18,11 +21,10 @@ public class Player extends Mob {
     // This value is used to change the amount of travel the player needs to do in a direction
     // before he actually rotates to that direction. Higher value means more travel
     private static final float DIRECTION_CHANGE_THRESHOLD = 2 / 3f;
+    // The base speed of the player, which is also the bonus for each speed upgrade
+    private static final float BASE_SPEED = 2;
     // A dampened velocity that never quite gets to the maximum speed of the player (asymptotic)
     private volatile Vector2f directionVelocity = Vector2f.ZERO;
-    private volatile int blastRadius = 1;
-    private volatile int placementBombCount = 1;
-    private volatile float speed = 3;
     private final Map<Class<? extends PowerUP>, Integer> powerUPs = new ConcurrentHashMap<>();
     private volatile SpriteInfo currentSprite = STILL_SPRITE;
 
@@ -36,21 +38,22 @@ public class Player extends Mob {
         // Only update the direction if we've been moving in that direction for a little while
         // This is a special case for player to improve rotation animations when going around corners
         directionVelocity = this.directionVelocity.add(velocity).div(2);
+        final float speed = getSpeed();
         if (directionVelocity.lengthSquared() / (speed * speed) >= DIRECTION_CHANGE_THRESHOLD) {
             direction = Direction.fromUnit(directionVelocity);
         }
     }
 
     public float getSpeed() {
-        return speed;
+        return (1 + getPowerUPLevel(SpeedUpgrade.class)) * BASE_SPEED;
     }
 
     public int getBlastRadius() {
-        return blastRadius;
+        return 1 + getPowerUPLevel(FlameUpgrade.class);
     }
 
-    public int getPlacementBombCount() {
-        return placementBombCount;
+    public int getBombPlacementCount() {
+        return 1 + getPowerUPLevel(BombUpgrade.class);
     }
 
     public Collection<Class<? extends PowerUP>> getPowerUPs() {
@@ -62,7 +65,11 @@ public class Player extends Mob {
     }
 
     public int getPowerUPLevel(Class<? extends PowerUP> powerUP) {
-        return powerUPs.get(powerUP);
+        final Integer level = powerUPs.get(powerUP);
+        if (level == null) {
+            return 0;
+        }
+        return level;
     }
 
     public void addPowerUP(PowerUP powerUP) {
