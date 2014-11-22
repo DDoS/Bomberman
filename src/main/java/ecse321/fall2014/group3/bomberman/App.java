2c27702b44fc8bec15687f06f9f3d4fba4b1ac3c
phil douyon <phild.email@gmail.com>
2014-11-22 17:16:49 -0500

    made a menu screen which has leaderboard inplemented.

--- src/main/java/ecse321/fall2014/group3/bomberman/App.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/App.java
@@ -5,10 +5,18 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
+import javax.swing.border.LineBorder;
+
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -30,12 +38,16 @@ public class App {
 
         final Session session = new Session();
         final Leaderboard leaderboard = new Leaderboard(session);
-        final JFrame frame = createLoginScreen(session, leaderboard);
-
+        final JFrame loginFrame = createLoginScreen(session, leaderboard);
 
         loginWait.acquireUninterruptibly();
 
-        frame.dispose();
+        loginFrame.dispose();
+
+        final JFrame menuFrame = createMenu(session, leaderboard);
+        loginWait.acquireUninterruptibly();
+        menuFrame.dispose();
+
         try {
             session.disconnect();
         } catch (SQLException e) {
@@ -51,30 +63,28 @@ public class App {
 
         JPanel panel = new JPanel(new GridLayout(20, 4));
         frame.add(panel, BorderLayout.CENTER);
-        
-        
-        
+
         JLabel userLabel = new JLabel("User");
         panel.add(userLabel);
-        
+
         final JTextField userText = new JTextField(10);
         panel.add(userText);
-        
+
         JLabel passwordLabel = new JLabel("Password");
         panel.add(passwordLabel);
 
         final JPasswordField passwordText = new JPasswordField(10);
         panel.add(passwordText);
-        
+
         JButton loginButton = new JButton("login");
         panel.add(loginButton);
         panel.getRootPane().setDefaultButton(loginButton);
-        
+
         panel.add(new JLabel());
-        
+
         JLabel newLabel = new JLabel("NEW USER");
         panel.add(newLabel);
-        
+
         panel.add(new JLabel());
 
         JLabel realNameLabel = new JLabel("Real Name");
@@ -82,28 +92,24 @@ public class App {
 
         final JTextField realNameText = new JTextField(10);
         panel.add(realNameText);
-        
+
         JLabel newUserLabel = new JLabel("User");
         panel.add(newUserLabel);
-        
+
         final JTextField newUserText = new JTextField(10);
         panel.add(newUserText);
-        
+
         JLabel newPasswordLabel = new JLabel("Password");
         panel.add(newPasswordLabel);
 
         final JPasswordField newPasswordText = new JPasswordField(10);
         panel.add(newPasswordText);
-        
-        
 
         JLabel verifyLabel = new JLabel("Verify Password");
         panel.add(verifyLabel);
 
         final JPasswordField verifyText = new JPasswordField(10);
         panel.add(verifyText);
-
-        
 
         JButton createButton = new JButton("New");
         panel.add(createButton);
@@ -116,16 +122,20 @@ public class App {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                    if (Login.login(userText.getText(), String.valueOf(passwordText.getPassword()), session)) {
-                        loginWait.release();
-                        session.create(userText.getText());
-                        leaderboard.updateScore(userText.getText(), (leaderboard.getScore(userText.getText()) +10));
-                    } else {
-                        passwordText.setText("");
-                        verifyText.setText("");
-                        error.setText("wrong username or password");
-                        error.setVisible(true);
-                    }
+                if (Login.login(userText.getText(), String.valueOf(passwordText.getPassword()), session)) {
+                    loginWait.release();
+                    session.create(userText.getText());
+                    leaderboard.updateScore(userText.getText(), (leaderboard.getScore(userText.getText()) + 10));
+
+                    //For testing purposes
+                    leaderboard.getTop(3);
+
+                } else {
+                    passwordText.setText("");
+                    verifyText.setText("");
+                    error.setText("wrong username or password");
+                    error.setVisible(true);
+                }
             }
         });
 
@@ -155,10 +165,126 @@ public class App {
             }
         });
 
-        frame.pack();
+        frame.setSize(300, 600);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
+        return frame;
+    }
+
+    private static JFrame createMenu(final Session session, final Leaderboard leaderboard) {
+        JFrame frame = new JFrame("Bomberman");
+        frame.setSize(300, 600);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JPanel mainPanel = new JPanel();
+        JPanel leaderboardPanel = new JPanel(new GridLayout(0, 2));
+        JPanel mainMenu = new JPanel(new GridLayout(10, 2));
+
+        final CardLayout card = new CardLayout();
+        mainPanel.setLayout(card);
+        mainPanel.add(mainMenu, "main");
+        mainPanel.add(leaderboardPanel, "leaderboard");
+
+        //create leaderboard panel
+
+        leaderboardPanel.setBackground(Color.LIGHT_GRAY);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        leaderboardPanel.add(userLabel);
+
+        JLabel scoreLabel = new JLabel("Score");
+        scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        leaderboardPanel.add(scoreLabel);
+
+        JLabel userName[] = new JLabel[10];
+        JLabel userScore[] = new JLabel[10];
+
+        String names[] = leaderboard.getTop(10);
+        int scores[] = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            userName[i] = new JLabel(names[i]);
+            userName[i].setHorizontalAlignment(SwingConstants.CENTER);
+            userName[i].setBorder(LineBorder.createBlackLineBorder());
+            leaderboardPanel.add(userName[i]);
+
+            if (names[i] != null) {
+                userScore[i] = new JLabel(String.valueOf(leaderboard.getScore(names[i])));
+            } else {
+                userScore[i] = new JLabel("");
+            }
+            userScore[i].setHorizontalAlignment(SwingConstants.CENTER);
+            userScore[i].setBorder(LineBorder.createBlackLineBorder());
+            leaderboardPanel.add(userScore[i]);
+        }
+
+        JButton returnButton = new JButton("Return");
+        leaderboardPanel.add(returnButton);
+
+        returnButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                card.show(mainPanel, "main");
+            }
+        });
+
+        //create main menu panel
+        mainMenu.setBackground(Color.LIGHT_GRAY);
+
+        JLabel bomberman = new JLabel("Bomberman");
+        bomberman.setHorizontalAlignment(SwingConstants.CENTER);
+        bomberman.setForeground(Color.black);
+        bomberman.setFont(new Font("Serif", Font.BOLD, 20));
+        mainMenu.add(bomberman);
+        mainMenu.add(new JLabel());
+
+        JButton continueButton = new JButton("Continue");
+        mainMenu.add(continueButton);
+        mainMenu.add(new JLabel());
+
+        JButton newGameButton = new JButton("New Game");
+        mainMenu.add(newGameButton);
+
+        //TODO: get max level
+        int maxLevel = 100;
+        SpinnerModel model = new SpinnerNumberModel(maxLevel, 0, maxLevel, 1);
+        JSpinner levelSelect = new JSpinner(model);
+        mainMenu.add(levelSelect);
+
+        mainMenu.add(new JLabel());
+
+        JButton leaderboardButton = new JButton("LeaderBoard");
+        mainMenu.add(leaderboardButton);
+        mainMenu.add(new JLabel());
+
+        leaderboardButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                card.show(mainPanel, "leaderboard");
+            }
+        });
+
+        continueButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //TODO: load previous session
+                //TODO: keep same size for game and menu
+                loginWait.release();
+            }
+        });
+
+        newGameButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //TODO: load game at level
+                //int value = (Integer) levelSelect.getValue();
+                loginWait.release();
+            }
+        });
+
+        card.show(mainPanel, "Main");
+        frame.add(mainPanel);
+        frame.setVisible(true);
         return frame;
     }
 }
