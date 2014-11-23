9c691615d181c115fc55109ee6253cefc57b1fc9
Marco Manglaviti <marco.manglaviti@gmail.com>
2014-11-23 17:05:51 -0500

    Added More Special Characters to Password Pattern

--- src/main/java/ecse321/fall2014/group3/bomberman/App.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/App.java
@@ -31,7 +31,7 @@ public class App {
 
     //1 Digit, 1 Upper, 1 Lower, 1 Special, Minimum length of 8
     private static final Pattern pattern =
-            Pattern.compile("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$&%]).{8,})");
+            Pattern.compile("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$&%!^*()-_=+]).{8,})");
 
     public static void main(String[] args) {
         LWJGLUtil.deployNatives(null);
