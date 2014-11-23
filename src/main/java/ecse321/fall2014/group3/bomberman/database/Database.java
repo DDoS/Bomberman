23816edd21b22cefd774fe4185737c64d8c5efe4
Aleksi Sapon <alek42008@yahoo.com>
2014-11-22 20:05:53 -0500

    Major database refactor part 2

--- src/main/java/ecse321/fall2014/group3/bomberman/database/Database.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/database/Database.java
@@ -19,216 +19,118 @@ public class Database {
     private final Connection connection;
 
     public Database() {
-
-        Connection c = null;
-
         try {
-
-            //opening database test
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:accounts.db");
-            System.out.println("Check Database Successfully");
-
-            c.setAutoCommit(false);
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            connection = DriverManager.getConnection("jdbc:sqlite:accounts.db");
+            connection.setAutoCommit(false);
+        } catch (ClassNotFoundException | SQLException exception) {
+            throw new RuntimeException("Couldn't open database connection", exception);
         }
-
-        connection = c;
-
-        try {
-
-            verifyTable("USERS");
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
+        verifyTable();
     }
 
     public Connection getConnection() {
         return connection;
     }
 
-    public void disconnect() throws SQLException {
-
+    public void disconnect() {
         try {
             connection.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } catch (SQLException exception) {
+            System.err.println("Couldn't close database connection");
+            exception.printStackTrace();
         }
     }
 
-    private void verifyTable(String table) {
-
-        Statement stmt;
-
-        try {
-
-            if (table.equals("USERS")) {
-
-                stmt = connection.createStatement();
-
-                String tbl = " CREATE TABLE IF NOT EXISTS USERS " +
-                        "(USERNAME       TEXT   NOT NULL," +
-                        " PASSWORD       TEXT   NOT NULL," +
-                        " REALNAME       TEXT   NOT NULL," +
-                        " SCORE          INT    NOT NULL," +
-                        " LEVEL          INT    NOT NULL)";
-
-                stmt.executeUpdate(tbl);
-
-                System.out.println("Checked USER Table Successfully");
-            }
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+    private void verifyTable() {
+        try (Statement statement = connection.createStatement()) {
+            final String sql = "CREATE TABLE IF NOT EXISTS USERS " +
+                    "(USERNAME  TEXT  NOT NULL  PRIMARY KEY," +
+                    " PASSWORD  TEXT  NOT NULL," +
+                    " REALNAME  TEXT  NOT NULL," +
+                    " SCORE     INT   NOT NULL," +
+                    " LEVEL     INT   NOT NULL)";
+            statement.executeUpdate(sql);
+            connection.commit();
+        } catch (SQLException exception) {
+            System.err.println("Couldn't verify user table");
+            exception.printStackTrace();
         }
     }
 
     public void setString(String username, String column, String value) {
-
-        PreparedStatement stmt;
-
-        try {
-
-            if (column.equals("USERNAME")) {
-                connection.setAutoCommit(false);
-
-                //that username does not exist
-
-                //create the username
-                String sql = "INSERT INTO USERS (USERNAME, PASSWORD, REALNAME, SCORE, LEVEL) " +
-                        " VALUES (?,?,?,?,?)";
-
-                stmt = connection.prepareStatement(sql);
-                stmt.setString(1, username);
-                stmt.setString(2, "");
-                stmt.setString(3, username);
-                stmt.setInt(4, 0);
-                stmt.setInt(5, 1);
-
-                stmt.executeUpdate();
-
+        if (column.equals(USERNAME_KEY)) {
+            final String sql = "INSERT INTO USERS (USERNAME, PASSWORD, REALNAME, SCORE, LEVEL) VALUES (?, ?, ?, ?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, username);
+                statement.setString(2, "");
+                statement.setString(3, username);
+                statement.setInt(4, 0);
+                statement.setInt(5, 1);
+                statement.executeUpdate();
                 connection.commit();
-                stmt.close();
-                connection.setAutoCommit(true);
-
-                System.out.println("Created Username");
-                System.out.println(username);
-            } else {
-
-                connection.setAutoCommit(false);
-
-                String sql = "UPDATE USERS SET " + column +
-                        "=? WHERE USERNAME=?";
-
-                stmt = connection.prepareStatement(sql);
-
-                stmt.setString(1, value);
-                stmt.setString(2, username);
-                stmt.executeUpdate();
-
-                connection.commit();
-                stmt.close();
-                connection.setAutoCommit(true);
-
-                System.out.println("Created " + column);
-                System.out.println(value);
+            } catch (SQLException exception) {
+                System.err.println("Couldn't create user row");
+                exception.printStackTrace();
             }
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } else {
+            final String sql = "UPDATE USERS SET " + column + " = ? WHERE USERNAME = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, value);
+                statement.setString(2, username);
+                statement.executeUpdate();
+                connection.commit();
+            } catch (SQLException exception) {
+                System.err.println("Couldn't update user row");
+                exception.printStackTrace();
+            }
         }
     }
 
     public void setInt(String username, String column, int value) {
-
-        PreparedStatement stmt;
-
-        try {
-
-            connection.setAutoCommit(false);
-
-            String sql = "UPDATE USERS SET " + column +
-                    "=? WHERE USERNAME=?";
-
-            stmt = connection.prepareStatement(sql);
-
-            stmt.setInt(1, value);
-            stmt.setString(2, username);
-            stmt.executeUpdate();
-
+        final String sql = "UPDATE USERS SET " + column + " = ? WHERE USERNAME = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, value);
+            statement.setString(2, username);
+            statement.executeUpdate();
             connection.commit();
-            stmt.close();
-            connection.setAutoCommit(true);
-
-            System.out.println("Updated " + column);
-            System.out.println(value);
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } catch (SQLException exception) {
+            System.err.println("Couldn't update user row");
+            exception.printStackTrace();
         }
     }
 
     public String getString(String username, String column) {
-
-        PreparedStatement stmt;
-
-        try {
-
-            connection.setAutoCommit(false);
-
-            //Check if username already exists
-            String check = "SELECT " + column + " FROM USERS WHERE USERNAME= ? ;";
-
-            stmt = connection.prepareStatement(check);
-            stmt.setString(1, username);
-
-            ResultSet rs = stmt.executeQuery();
-
+        final String sql = "SELECT " + column + " FROM USERS WHERE USERNAME = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, username);
+            final ResultSet rs = statement.executeQuery();
+            connection.commit();
             if (!rs.next()) {
-
-                System.out.println("No results found for " + column);
                 return null;
             }
-
             return rs.getString(column);
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } catch (SQLException exception) {
+            System.err.println("Couldn't get user column");
+            exception.printStackTrace();
         }
-
         return null;
     }
 
     public int getInt(String username, String column) {
-
-        PreparedStatement stmt;
-
-        try {
-
-            connection.setAutoCommit(false);
-
-            String check = "SELECT " + column + " FROM USERS WHERE USERNAME= ? ;";
-
-            stmt = connection.prepareStatement(check);
-            stmt.setString(1, username);
-
-            ResultSet rs = stmt.executeQuery();
-
+        final String sql = "SELECT " + column + " FROM USERS WHERE USERNAME = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, username);
+            final ResultSet rs = statement.executeQuery();
+            connection.commit();
             if (!rs.next()) {
-
-                System.out.println("No results found for " + column);
                 return -1;
             }
-
             return rs.getInt(column);
-        } catch (Exception e) {
-
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } catch (SQLException exception) {
+            System.err.println("Couldn't get user column");
+            exception.printStackTrace();
         }
-
         return -1;
     }
 }
