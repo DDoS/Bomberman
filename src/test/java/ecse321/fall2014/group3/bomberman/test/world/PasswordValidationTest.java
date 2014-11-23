e6b1eba38f147020479d1a05505d86971053e103
Marco Manglaviti <marco.manglaviti@gmail.com>
2014-11-23 17:44:28 -0500

    Added basic Unit Tests for Password Validation

---
+++ src/test/java/ecse321/fall2014/group3/bomberman/test/world/PasswordValidationTest.java
@@ -0,0 +1,35 @@
+package ecse321.fall2014.group3.bomberman.test.world;
+
+import ecse321.fall2014.group3.bomberman.App;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Marco Manglaviti on 23/11/2014.
+ */
+public class PasswordValidationTest {
+
+    @Test
+    public void test() {
+
+        //Tests that should be successful
+        Assert.assertTrue(App.validatePassword("abc123A@"));
+        Assert.assertTrue(App.validatePassword("aaaaa1A@"));
+        Assert.assertTrue(App.validatePassword("aA#12345"));
+        Assert.assertTrue(App.validatePassword("a1@ABCDC"));
+        Assert.assertTrue(App.validatePassword("a1A@#$%^&*("));
+        Assert.assertTrue(App.validatePassword("a1A^^^()(^^"));
+
+        //Tests that should fail
+        Assert.assertFalse(App.validatePassword("abc123"));
+        Assert.assertFalse(App.validatePassword(""));
+        Assert.assertFalse(App.validatePassword("aA2@"));
+        Assert.assertFalse(App.validatePassword("aaaaaaaa"));
+        Assert.assertFalse(App.validatePassword("123456789"));
+        Assert.assertFalse(App.validatePassword("ABCDCEDF"));
+        Assert.assertFalse(App.validatePassword("!@#$%^&*()_"));
+
+    }
+}
+
+
