53ff1dfd46d52127e81a8b7d17c9febd93110e92
Aleksi Sapon <QcTechs@gmail.com>
2014-11-23 21:46:21 -0500

    Add code for enemies in physics
    Right now just spawn 1 Balloom
    Also, cleanup of enemy classes

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/DumbAI.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/DumbAI.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 import com.flowpowered.math.vector.Vector2f;
 
-import ecse321.fall2014.group3.bomberman.physics.entity.mob.Mob;
+import ecse321.fall2014.group3.bomberman.physics.entity.Entity;
 import ecse321.fall2014.group3.bomberman.physics.entity.mob.Player;
 import ecse321.fall2014.group3.bomberman.world.Map;
 import ecse321.fall2014.group3.bomberman.world.tile.Tile;
@@ -14,52 +14,48 @@ import ecse321.fall2014.group3.bomberman.world.tile.Tile;
  *
  */
 public class DumbAI extends AI {
-   public DumbAI(Mob target) {
-      super(target);
-   }
+    @Override
+    public Vector2f nextPosition(Entity target, long dt, Map map, Player player) {
+        float timeSec = dt / TO_SECS;
+        Vector2f enemyPos = target.getPosition();
+        Vector2f eVelocity = target.getVelocity();
+        Set<Tile> colTiles = target.getTileCollisionList();
 
-   @Override
-   public Vector2f nextPosition(long dt, Map map, Player player) {
-      float timeSec = dt / TO_SECS;
-      Vector2f enemyPos = target.getPosition();
-      Vector2f eVelocity = target.getVelocity();
-      Set<Tile> colTiles = target.getTileCollisionList();
-
-      if (eVelocity.length() == 0) {
-         return enemyPos;
-      } else if (colTiles.isEmpty()) {
-         Random random = new Random();
-         switch (random.nextInt(4)) {
-            case 0:
-               return enemyPos.sub(eVelocity.getX() * timeSec, 0f);
-            case 1:
-               return enemyPos.sub(0f, eVelocity.getY() * timeSec);
-            case 2:
-               return enemyPos.add(0f, eVelocity.getY() * timeSec);
-            case 3:
-               return enemyPos.add(eVelocity.getX() * timeSec, 0f);
-         }
-      } else {
-         boolean frontCollide = false;
-
-         for (Tile tile : colTiles) {
-            if ((tile.getPosition()).dot(enemyPos) > 0) {
-               frontCollide = true;
-            }
-         }
-
-         if (frontCollide) {
+        if (eVelocity.length() == 0) {
+            return enemyPos;
+        } else if (colTiles.isEmpty()) {
             Random random = new Random();
-            switch (random.nextInt(3)) {
-               case 0:
-                  return enemyPos.add(0f, eVelocity.getY() * timeSec);
-               case 1:
-                  return enemyPos.sub(0f, eVelocity.getY() * timeSec);
-               case 2:
-                  return enemyPos.sub(eVelocity.getX() * timeSec, 0f);
+            switch (random.nextInt(4)) {
+                case 0:
+                    return enemyPos.sub(eVelocity.getX() * timeSec, 0f);
+                case 1:
+                    return enemyPos.sub(0f, eVelocity.getY() * timeSec);
+                case 2:
+                    return enemyPos.add(0f, eVelocity.getY() * timeSec);
+                case 3:
+                    return enemyPos.add(eVelocity.getX() * timeSec, 0f);
             }
-         }
-      }
-      return target.getPosition();
-   }
+        } else {
+            boolean frontCollide = false;
+
+            for (Tile tile : colTiles) {
+                if ((tile.getPosition()).dot(enemyPos) > 0) {
+                    frontCollide = true;
+                }
+            }
+
+            if (frontCollide) {
+                Random random = new Random();
+                switch (random.nextInt(3)) {
+                    case 0:
+                        return enemyPos.add(0f, eVelocity.getY() * timeSec);
+                    case 1:
+                        return enemyPos.sub(0f, eVelocity.getY() * timeSec);
+                    case 2:
+                        return enemyPos.sub(eVelocity.getX() * timeSec, 0f);
+                }
+            }
+        }
+        return target.getPosition();
+    }
 }
--- src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/RegularAI.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/RegularAI.java
@@ -2,7 +2,7 @@ package ecse321.fall2014.group3.bomberman.physics.ai;
 
 import com.flowpowered.math.vector.Vector2f;
 
-import ecse321.fall2014.group3.bomberman.physics.entity.mob.Mob;
+import ecse321.fall2014.group3.bomberman.physics.entity.Entity;
 import ecse321.fall2014.group3.bomberman.physics.entity.mob.Player;
 import ecse321.fall2014.group3.bomberman.world.Map;
 
@@ -10,12 +10,8 @@ import ecse321.fall2014.group3.bomberman.world.Map;
  *
  */
 public class RegularAI extends AI {
-    public RegularAI(Mob target) {
-        super(target);
-    }
-
     @Override
-    public Vector2f nextPosition(long dt, Map map, Player player) {
+    public Vector2f nextPosition(Entity target, long dt, Map map, Player player) {
         // TODO: implement me, for now just return the same position
         return target.getPosition();
     }
