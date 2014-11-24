0202c828a84934b023039b6fb017372ee3f24dec
Kanda <liannayangse@gmail.com>
2014-11-24 16:09:03 -0500

    Finished DumbAI, changed so path decision is made based off map tiles not collisions

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/DumbAI.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/DumbAI.java
@@ -8,54 +8,83 @@ import com.flowpowered.math.vector.Vector2f;
 import ecse321.fall2014.group3.bomberman.physics.entity.Entity;
 import ecse321.fall2014.group3.bomberman.physics.entity.mob.Player;
 import ecse321.fall2014.group3.bomberman.world.Map;
+import ecse321.fall2014.group3.bomberman.world.tile.Air;
 import ecse321.fall2014.group3.bomberman.world.tile.Tile;
 
 /**
  *
  */
 public class DumbAI extends AI {
-    @Override
-    public Vector2f nextPosition(Entity target, long dt, Map map, Player player) {
-        float timeSec = dt / TO_SECS;
-        Vector2f enemyPos = target.getPosition();
-        Vector2f eVelocity = target.getVelocity();
-        Set<Tile> colTiles = target.getTileCollisionList();
-
-        if (eVelocity.length() == 0) {
-            return enemyPos;
-        } else if (colTiles.isEmpty()) {
-            Random random = new Random();
-            switch (random.nextInt(4)) {
-                case 0:
-                    return enemyPos.sub(eVelocity.getX() * timeSec, 0f);
-                case 1:
-                    return enemyPos.sub(0f, eVelocity.getY() * timeSec);
-                case 2:
-                    return enemyPos.add(0f, eVelocity.getY() * timeSec);
-                case 3:
-                    return enemyPos.add(eVelocity.getX() * timeSec, 0f);
+   @Override
+   public Vector2f nextPosition(Entity target, long dt, Map map, Player player) {
+      float timeSec = dt / TO_SECS;
+      Vector2f enemyPos = target.getPosition();
+      Vector2f eVelocity = target.getVelocity();
+      Set<Tile> colTiles = target.getTileCollisionList();
+   
+      if (eVelocity.length() == 0) {
+         if (map.isTile(enemyPos.add(1f, 0f), Air.class)) {
+            target.setVelocity(new Vector2f(1f, 0f));
+            return enemyPos.add(1f * timeSec, 0f);
+         }
+         else if (map.isTile(enemyPos.sub(1f, 0f), Air.class)) {
+            target.setVelocity(new Vector2f(-1f, 0f));
+            return enemyPos.sub(1f * timeSec, 0f);
+         }
+         else if (map.isTile(enemyPos.add(0f, 1f), Air.class)) {
+            target.setVelocity(new Vector2f(0f, 1f));
+            return enemyPos.add(0f, 1f * timeSec);
+         }
+         else {
+            target.setVelocity(new Vector2f(0f, -1f));
+            return enemyPos.sub(0f, 1f * timeSec);
+         }
+      } 
+      else {
+         float xs = eVelocity.getX();
+         float ys = eVelocity.getY();
+         
+         if (xs == 0f) {
+            if (ys < 0) {
+               if (map.isTile(enemyPos.add(0f, timeSec * ys), Air.class)) {
+                  return enemyPos.add(0f, ys * timeSec);
+               }
+               else {
+                  target.setVelocity(new Vector2f(0f, 1f));
+                  return enemyPos.add(0f, 1f * timeSec);
+               }
             }
-        } else {
-            boolean frontCollide = false;
-
-            for (Tile tile : colTiles) {
-                if ((tile.getPosition()).dot(enemyPos) > 0) {
-                    frontCollide = true;
-                }
+            else {
+               if (map.isTile(enemyPos.add(0f, ys), Air.class)) {
+                  return enemyPos.add(0f, ys * timeSec);
+               }
+               else {
+                  target.setVelocity(new Vector2f(0f, -1f));
+                  return enemyPos.add(0f, -1f * timeSec);
+               }
             }
-
-            if (frontCollide) {
-                Random random = new Random();
-                switch (random.nextInt(3)) {
-                    case 0:
-                        return enemyPos.add(0f, eVelocity.getY() * timeSec);
-                    case 1:
-                        return enemyPos.sub(0f, eVelocity.getY() * timeSec);
-                    case 2:
-                        return enemyPos.sub(eVelocity.getX() * timeSec, 0f);
-                }
+         }
+         else {
+            if (xs < 0) {
+               if (map.isTile(enemyPos.add(xs * timeSec, 0f), Air.class)) {
+                  return enemyPos.add(xs * timeSec, 0f);
+               }
+               else {
+                  target.setVelocity(new Vector2f(1f, 0f));
+                  return enemyPos.add(1f * timeSec, 0f);
+               }
             }
-        }
-        return target.getPosition();
-    }
+            else {
+               if (map.isTile(enemyPos.add(xs, 0f), Air.class)) {
+                  return enemyPos.add(xs * timeSec, 0f);
+               }
+               else {
+                  target.setVelocity(new Vector2f(-1f, 0f));
+                  return enemyPos.add(-1f * timeSec, 0f);
+               }
+            }
+         }
+      } 
+      //return target.getPosition();
+   }
 }
