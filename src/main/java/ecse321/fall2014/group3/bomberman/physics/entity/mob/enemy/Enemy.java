7d385027216bf779130c196524dc41e8ed220485
Aleksi Sapon <QcTechs@gmail.com>
2014-11-23 21:49:56 -0500

    Make Enemy game overs a bit more forgiving
    Allow for more collision to occur before the game over

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/mob/enemy/Enemy.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/mob/enemy/Enemy.java
@@ -11,7 +11,7 @@ import ecse321.fall2014.group3.bomberman.physics.entity.mob.Mob;
  */
 public abstract class Enemy extends Mob {
     private static final Vector2f SIZE = Vector2f.ONE;
-    private static final CollisionBox COLLISION_BOX = new CollisionBox(SIZE);
+    private static final CollisionBox COLLISION_BOX = new CollisionBox(SIZE.mul(0.8f));
     public static final float ENEMY_BASE_SPEED = 2;
 
     public Enemy(Vector2f position) {
