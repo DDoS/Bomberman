3f59eda5194343b6a3a954bf5a39e1da678c7038
Kanda <liannayangse@gmail.com>
2014-11-29 16:19:24 -0500

    Added Wallpass for DumbAI

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/DumbAI.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/DumbAI.java
@@ -7,6 +7,7 @@ import ecse321.fall2014.group3.bomberman.physics.entity.mob.enemy.Enemy;
 import ecse321.fall2014.group3.bomberman.world.Map;
 import ecse321.fall2014.group3.bomberman.world.tile.Air;
 import ecse321.fall2014.group3.bomberman.world.tile.timed.Fire;
+import ecse321.fall2014.group3.bomberman.world.tile.wall.Breakable;
 
 /**
  *
@@ -33,7 +34,7 @@ public class DumbAI extends AI {
                 target.setVelocity(new Vector2f(0f, -eSpeed));
                 return enemyPos.sub(0f, eSpeed * timeSec);
             }
-        } else {
+        } else if (!target.isWallPass()) {
             float xs = eVelocity.getX();
             float ys = eVelocity.getY();
 
@@ -72,6 +73,59 @@ public class DumbAI extends AI {
                         return enemyPos.add(xs * timeSec, 0f);
                     } else if (map.isTile(enemyPos.add(1f, 0f), Fire.class)) {
                         return enemyPos.add(xs * timeSec, 0f);
+                    } else {
+                        target.setVelocity(new Vector2f(-eSpeed, 0f));
+                        return enemyPos.add(-eSpeed * timeSec, 0f);
+                    }
+                }
+            }        
+        } else {
+            float xs = eVelocity.getX();
+            float ys = eVelocity.getY();
+
+            if (xs == 0f) {
+                if (ys < 0) {
+                    if (map.isTile(enemyPos.add(0f, timeSec * ys), Air.class) ||
+                        map.isTile(enemyPos.add(0f, timeSec * ys), Breakable.class) ||
+                        map.isTile(enemyPos.add(0f, timeSec * ys), Fire.class)) {
+                        return enemyPos.add(0f, ys * timeSec);
+                    /*} /*else if (map.isTile(enemyPos.add(0f, timeSec * ys), Fire.class)) {
+                        return enemyPos.add(0f, ys * timeSec);*/
+                    } else {
+                        target.setVelocity(new Vector2f(0f, eSpeed));
+                        return enemyPos.add(0f, eSpeed * timeSec);
+                    }
+                } else {
+                    if (map.isTile(enemyPos.add(0f, 1f), Air.class) ||
+                        map.isTile(enemyPos.add(0f, 1f), Breakable.class) ||
+                        map.isTile(enemyPos.add(0f, 1f), Fire.class)) {
+                        return enemyPos.add(0f, ys * timeSec);
+                    /*} /*else if (map.isTile(enemyPos.add(0f, 1f), Fire.class)) {
+                        return enemyPos.add(0f, ys * timeSec);*/
+                    } else {
+                        target.setVelocity(new Vector2f(0f, -eSpeed));
+                        return enemyPos.add(0f, -eSpeed * timeSec);
+                    }
+                }
+            } else {
+                if (xs < 0) {
+                    if (map.isTile(enemyPos.add(xs * timeSec, 0f), Air.class) ||
+                        map.isTile(enemyPos.add(xs * timeSec, 0f), Breakable.class) ||
+                        map.isTile(enemyPos.add(xs * timeSec, 0f), Fire.class)) {
+                        return enemyPos.add(xs * timeSec, 0f);
+                    /*} else if (map.isTile(enemyPos.add(xs * timeSec, 0f), Fire.class)) {
+                        return enemyPos.add(xs * timeSec, 0f);*/
+                    } else {
+                        target.setVelocity(new Vector2f(eSpeed, 0f));
+                        return enemyPos.add(eSpeed * timeSec, 0f);
+                    }
+                } else {
+                    if (map.isTile(enemyPos.add(1f, 0f), Air.class) ||
+                        map.isTile(enemyPos.add(1f, 0f), Breakable.class) ||
+                        map.isTile(enemyPos.add(1f, 0f), Fire.class)) {
+                        return enemyPos.add(xs * timeSec, 0f);
+                    /*} else if (map.isTile(enemyPos.add(1f, 0f), Fire.class)) {
+                        return enemyPos.add(xs * timeSec, 0f);*/
                     } else {
                         target.setVelocity(new Vector2f(-eSpeed, 0f));
                         return enemyPos.add(-eSpeed * timeSec, 0f);
