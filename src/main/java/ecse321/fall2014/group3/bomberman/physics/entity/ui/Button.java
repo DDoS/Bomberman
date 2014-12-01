f257fecbe85494f6d41a8498f1972c724e76ce81
phil douyon <phild.email@gmail.com>
2014-12-01 12:17:32 -0500

    javadoc for ui

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/ui/Button.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/ui/Button.java
@@ -1,15 +1,30 @@
+/**
+ * @author Phil Douyon
+ */
 package ecse321.fall2014.group3.bomberman.physics.entity.ui;
 
 import com.flowpowered.math.vector.Vector2f;
+
 import com.flowpowered.math.vector.Vector4f;
 
 import org.spout.renderer.api.util.CausticUtil;
 
+/**
+ * The Class Button.
+ */
 public class Button extends TextBox {
     private final String[] action;
     private volatile boolean selected = false;
     private volatile Vector4f selectedColor = CausticUtil.YELLOW;
 
+    /**
+     * Instantiates a new button.
+     *
+     * @param position the position
+     * @param size the size
+     * @param text the text
+     * @param action the action
+     */
     public Button(Vector2f position, Vector2f size, String text, String action) {
         super(position, size, text);
         this.action = action.split("\\.");
@@ -18,22 +33,45 @@ public class Button extends TextBox {
         }
     }
 
+    /**
+     * Gets the action.
+     *
+     * @return the action
+     */
     public String[] getAction() {
         return action;
     }
 
+    /**
+     * Checks if is selected.
+     *
+     * @return true, if is selected
+     */
     public boolean isSelected() {
         return selected;
     }
 
+    /**
+     * Sets the selected.
+     *
+     * @param selected the new selected
+     */
     public void setSelected(boolean selected) {
         this.selected = selected;
     }
 
+    /**
+     * Sets the selected color.
+     *
+     * @param selectedColor the new selected color
+     */
     public void setSelectedColor(Vector4f selectedColor) {
         this.selectedColor = selectedColor;
     }
 
+    /* (non-Javadoc)
+     * @see ecse321.fall2014.group3.bomberman.physics.entity.ui.TextBox#getTextColor()
+     */
     @Override
     public Vector4f getTextColor() {
         if (selected) {
--- src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/ui/UIBox.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/entity/ui/UIBox.java
@@ -1,3 +1,6 @@
+/**
+ * @author Phil Douyon
+ */
 package ecse321.fall2014.group3.bomberman.physics.entity.ui;
 
 import com.flowpowered.math.vector.Vector2f;
@@ -8,19 +11,32 @@ import ecse321.fall2014.group3.bomberman.physics.CollisionBox;
 import ecse321.fall2014.group3.bomberman.physics.entity.Entity;
 
 /**
- *
+ * The Class UIBox.
  */
 public abstract class UIBox extends Entity implements TextTextured {
     private static final FontInfo FONT = new FontInfo("Arial Bold", 16);
+
+    /**
+     * Instantiates a new UI box.
+     *
+     * @param position the position
+     * @param size the size
+     */
     public UIBox(Vector2f position, Vector2f size) {
         super(position, new CollisionBox(size));
     }
 
+    /* (non-Javadoc)
+     * @see ecse321.fall2014.group3.bomberman.nterface.Textured#getModelSize()
+     */
     @Override
     public Vector2f getModelSize() {
         return collisionBox.getSize();
     }
 
+    /* (non-Javadoc)
+     * @see ecse321.fall2014.group3.bomberman.nterface.TextTextured#getFontInfo()
+     */
     @Override
     public FontInfo getFontInfo() {
         return FONT;
