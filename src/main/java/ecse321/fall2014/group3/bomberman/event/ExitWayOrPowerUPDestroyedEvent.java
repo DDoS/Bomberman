a52f8a154a437f74cedb31208e5f14f612d02f17
phil douyon <phild.email@gmail.com>
2014-12-01 12:46:27 -0500

    javadoc for events

--- src/main/java/ecse321/fall2014/group3/bomberman/event/ExitWayOrPowerUPDestroyedEvent.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/event/ExitWayOrPowerUPDestroyedEvent.java
@@ -1,3 +1,6 @@
+/**
+ * @author Phil Douyon
+ */
 package ecse321.fall2014.group3.bomberman.event;
 
 import ecse321.fall2014.group3.bomberman.world.tile.ExitWay;
@@ -5,11 +8,16 @@ import ecse321.fall2014.group3.bomberman.world.tile.Tile;
 import ecse321.fall2014.group3.bomberman.world.tile.powerup.PowerUP;
 
 /**
- *
+ * The Class ExitWayOrPowerUPDestroyedEvent.
  */
 public class ExitWayOrPowerUPDestroyedEvent extends Event {
     private final boolean exitWay;
 
+    /**
+     * Instantiates a new exitWay or powerUP destroyed event.
+     *
+     * @param destroyed the destroyed tile
+     */
     public ExitWayOrPowerUPDestroyedEvent(Tile destroyed) {
         exitWay = destroyed instanceof ExitWay;
         if (!exitWay && !(destroyed instanceof PowerUP)) {
@@ -17,10 +25,20 @@ public class ExitWayOrPowerUPDestroyedEvent extends Event {
         }
     }
 
+    /**
+     * Checks if is exitWay tile.
+     *
+     * @return true, if is exitWay tile
+     */
     public boolean isExitWay() {
         return exitWay;
     }
 
+    /**
+     * Checks if is powerUP tile.
+     *
+     * @return true, if is powerUp tile
+     */
     public boolean isPowerUP() {
         return !isExitWay();
     }
