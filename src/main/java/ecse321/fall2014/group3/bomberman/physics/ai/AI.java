3ad8ff99ac16815766a255fe2e16d081171769b2
Aleksi Sapon <alek42008@yahoo.com>
2014-12-01 15:07:00 -0500

    Improve AI JD

--- src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/AI.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/physics/ai/AI.java
@@ -7,23 +7,23 @@ import ecse321.fall2014.group3.bomberman.physics.entity.mob.enemy.Enemy;
 import ecse321.fall2014.group3.bomberman.world.Map;
 
 /**
- * The AI superclass.  Contains the float for conversion of the poll time from seconds to nanoseconds 
+ * The AI superclass.
  *
- * @author 
+ * @author Lianna Yang
  */
 public abstract class AI {
     /**
-    * Float used to convert the poll time from seconds to nanoseconds
+    * Float used to convert the poll time from nanoseconds to seconds
     */
     protected static final float TO_SECS = 1e9f;
     
     /**
     * Returns a Vector2f that represents the next position of the Enemy
     * 
-    * @param Enemy target
-    * @param long dt
-    * @param Map map
-    * @param Player player
+    * @param target The enemy to apply the AI on
+    * @param dt The time since the last AI update
+    * @param map The map in which the enemy and player are
+    * @param player The player, to chase if needed
     * @return Vector2f The next position of the Enemy
     */
     public abstract Vector2f nextPosition(Enemy target, long dt, Map map, Player player);
