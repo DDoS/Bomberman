ae2f40769bfded0f4c1e6bf31d3b6caaa137f783
phil douyon <phild.email@gmail.com>
2014-12-01 12:02:16 -0500

    added javadoc for walls

--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/wall/Breakable.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/wall/Breakable.java
@@ -1,3 +1,6 @@
+/**
+ * @author Phil Douyon
+ */
 package ecse321.fall2014.group3.bomberman.world.tile.wall;
 
 import com.flowpowered.math.vector.Vector2f;
@@ -5,13 +8,24 @@ import com.flowpowered.math.vector.Vector2f;
 import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
 import ecse321.fall2014.group3.bomberman.world.tile.CollidableTile;
 
+/**
+ * The Class Breakable tile.
+ */
 public class Breakable extends CollidableTile {
     private static final SpriteInfo BREAKABLE_SPRITE = new SpriteInfo("Sprite", 65, Vector2f.ONE);
 
+    /**
+     * Instantiates a new breakable tile.
+     *
+     * @param position the position
+     */
     public Breakable(Vector2f position) {
         super(position);
     }
 
+    /* (non-Javadoc)
+     * @see ecse321.fall2014.group3.bomberman.nterface.SpriteTextured#getSpriteInfo()
+     */
     @Override
     public SpriteInfo getSpriteInfo() {
         return BREAKABLE_SPRITE;
