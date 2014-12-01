419f516636cf12fec0e2d30f5732cf8f045608f3
phil douyon <phild.email@gmail.com>
2014-12-01 12:00:43 -0500

    added javadoc for timed tiles

--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/timed/Bomb.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/timed/Bomb.java
@@ -1,24 +1,41 @@
+/**
+ * @author Phil Douyon
+ */
 package ecse321.fall2014.group3.bomberman.world.tile.timed;
 
 import com.flowpowered.math.vector.Vector2f;
 
 import ecse321.fall2014.group3.bomberman.nterface.SpriteInfo;
 
+/**
+ * The Class Bomb tile.
+ */
 public class Bomb extends TimedTile {
     private static final SpriteInfo BOMB_SPRITE = new SpriteInfo("Sprite", 68, Vector2f.ONE);
     private static final long LIFE_TIME = 3000;
     private static final float ANIMATION_GROWTH_PERCENT = 0.25f;
 
+    /**
+     * Instantiates a new bomb tile.
+     *
+     * @param position the position
+     */
     public Bomb(Vector2f position) {
         super(position, LIFE_TIME);
     }
 
+    /* (non-Javadoc)
+     * @see ecse321.fall2014.group3.bomberman.world.tile.Tile#getModelSize()
+     */
     @Override
     public Vector2f getModelSize() {
         final float timePercent = 1 - getRemainingTime() / (float) LIFE_TIME;
         return super.getModelSize().mul(timePercent * ANIMATION_GROWTH_PERCENT + (1 - ANIMATION_GROWTH_PERCENT));
     }
 
+    /* (non-Javadoc)
+     * @see ecse321.fall2014.group3.bomberman.nterface.SpriteTextured#getSpriteInfo()
+     */
     @Override
     public SpriteInfo getSpriteInfo() {
         return BOMB_SPRITE;
--- src/main/java/ecse321/fall2014/group3/bomberman/world/tile/timed/TimedTile.java
+++ src/main/java/ecse321/fall2014/group3/bomberman/world/tile/timed/TimedTile.java
@@ -1,28 +1,57 @@
+/**
+ * @author Phil Douyon
+ */
 package ecse321.fall2014.group3.bomberman.world.tile.timed;
 
 import com.flowpowered.math.vector.Vector2f;
 
 import ecse321.fall2014.group3.bomberman.world.tile.CollidableTile;
 
+/**
+ * The Class TimedTile.
+ */
 public abstract class TimedTile extends CollidableTile {
     private final long lifeTime, placeTime;
 
+    /**
+     * Instantiates a new timed tile.
+     *
+     * @param position the position
+     * @param size the size
+     * @param lifeTime the life time
+     */
     protected TimedTile(Vector2f position, float size, long lifeTime) {
         super(position, size);
         this.lifeTime = lifeTime;
         placeTime = System.currentTimeMillis();
     }
 
+    /**
+     * Instantiates a new timed tile.
+     *
+     * @param position the position
+     * @param lifeTime the life time
+     */
     protected TimedTile(Vector2f position, long lifeTime) {
         super(position);
         this.lifeTime = lifeTime;
         placeTime = System.currentTimeMillis();
     }
 
+    /**
+     * Gets the remaining time.
+     *
+     * @return the remaining time
+     */
     public long getRemainingTime() {
         return Math.max(lifeTime - (System.currentTimeMillis() - placeTime), 0);
     }
 
+    /**
+     * Checks for expired.
+     *
+     * @return true, if successful
+     */
     public boolean hasExpired() {
         return getRemainingTime() <= 0;
     }
